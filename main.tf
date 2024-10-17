b028af1a8616f66747ee44565fad84e5ff4fbcae
erickkendall <ekendallgithub@gmail.com>
2024-10-17 18:44:32 -0500

    Add DynamoDb table.

--- main.tf
+++ main.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "nurple"
-    key    = "terraform/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "nurple"
+    key            = "terraform/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
   }
 }
