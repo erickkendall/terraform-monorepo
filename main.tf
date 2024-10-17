2b2a70d412a4182907685ceebbeca2dbe969105b
erickkendall <ekendallgithub@gmail.com>
2024-10-17 18:51:21 -0500

    Add creation of EC2 instance.

--- main.tf
+++ main.tf
@@ -1,4 +1,14 @@
+# Specify the required Terraform version
 terraform {
+  required_version = ">= 1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
   backend "s3" {
     bucket         = "nurple"
     key            = "terraform/terraform.tfstate"
@@ -6,4 +16,45 @@ terraform {
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
   }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Data source to get the latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
+
+# Output the instance ID
+output "instance_id" {
+  value = aws_instance.example.id
+}
+
+# Output the public IP address
+output "public_ip" {
+  value = aws_instance.example.public_ip
 }
